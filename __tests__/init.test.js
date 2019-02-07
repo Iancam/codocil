fd3a205bb430f3e68c6d367e779c0bb2d6a67abc
Ian Campbell <iancam92@icloud.com>
2019-02-07 14:45:05 -0800

    tests no longer overwrite one another through unique ids. Using a tmp file, but should use mock-fs

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -5,17 +5,13 @@ const config = require('../src/codocil.config')
 const { setup } = require('../src/test_utils')
 
 test('generates correct file', async () => {
-  const projectName = 'testDir'
-  const { cli, tearDown } = setup(projectName)
+  const projectName = 'testtDir'
+  const { cli, tearDown, itermocilFilePath } = setup(projectName)
 
   const output = await cli('init')
   expect(output).toContain('Found 1 projects')
   expect(output).toContain(projectName)
 
-  const itermocilFilePath = join(
-    config.defaults.projectDirectory,
-    projectName + '.yml'
-  )
   expect(filesystem.exists(itermocilFilePath)).toBe('file')
   const itermocilFile = filesystem.read(itermocilFilePath)
 
--- src/codocil.config.js
+++ src/codocil.config.js
@@ -4,6 +4,43 @@ const itermocil = join(require('os').homedir(), '.itermocil')
 module.exports = {
   name: 'codocil',
   defaults: {
+    packageContents: {
+      name: 'codocil',
+      version: '0.0.1',
+      description: 'codocil CLI',
+      private: true,
+      bin: {
+        codocil: 'bin/codocil'
+      },
+      scripts: {
+        format: 'prettier --write **/*.{js,json} && standard --fix',
+        lint: 'standard',
+        test: 'jest',
+        watch: 'jest --watch --verbose=false',
+        snapupdate: 'jest --updateSnapshot',
+        coverage: 'jest --coverage'
+      },
+      license: 'MIT',
+      dependencies: {
+        gluegun: 'next',
+        lodash: 'next'
+      },
+      devDependencies: {
+        jest: '^24.0.0-alpha.16',
+        prettier: '^1.12.1',
+        standard: '^12.0.1'
+      },
+      jest: {
+        testEnvironment: 'node'
+      },
+      standard: {
+        env: ['jest']
+      },
+      prettier: {
+        semi: false,
+        singleQuote: true
+      }
+    },
     itermocilDirectory: itermocil,
     projectDirectory: join(itermocil, '.codocil'),
     ignoredDirectories: [
